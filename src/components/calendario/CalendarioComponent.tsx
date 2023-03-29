27d999c98efc29821fcb54dbbdd29e9647ae4c28
negrativo <lucas.rsouza2000@gmail.com>
2023-03-29 12:33:34 -0300

    adicionado calendario a agendamento

--- src/components/calendario/CalendarioComponent.tsx
+++ src/components/calendario/CalendarioComponent.tsx
@@ -2,42 +2,41 @@ import React, { useState } from 'react';
 import { View } from 'react-native';
 import moment from 'moment';
 import styles from './StyleCalendarioComponent';
+import { Calendar, DateData, LocaleConfig } from 'react-native-calendars';
 
 export default function () {
   const _format = 'YYYY-MM-DD';
   const _today = moment().format(_format);
   const [markedDates, setMarkedDates] = useState({ [_today]: {} })
 
-  // LocaleConfig.locales['br'] = {
-  //   monthNames: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
-  //   monthNamesShort: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
-  //   dayNames: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sabado'],
-  //   dayNamesShort: ['Dom', 'Seg.', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab'],
-  //   today: 'Hoje'
-  // };
-  // LocaleConfig.defaultLocale = 'br';
+  LocaleConfig.locales['br'] = {
+    monthNames: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+    monthNamesShort: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+    dayNames: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sabado'],
+    dayNamesShort: ['Dom', 'Seg.', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab'],
+    today: 'Hoje'
+  };
+  LocaleConfig.defaultLocale = 'br';
 
-  // function onDaySelect(day: DateData) {
-  //   const _selectedDay = moment(day.dateString).format(_format);
+  function onDaySelect(day: DateData) {
+    const _selectedDay = moment(day.dateString).format(_format);
 
-  //   let marked = true;
-  //   if (markedDates[_selectedDay]) {
-  //     marked = !markedDates[_selectedDay];
-  //   }
+    let marked = true;
+    if (markedDates[_selectedDay]) {
+      marked = !markedDates[_selectedDay];
+    }
 
-  //   const updatedMarkedDates = { ...markedDates, ...{ [_selectedDay]: { marked } } }
+    const updatedMarkedDates = { ...markedDates, ...{ [_selectedDay]: { marked } } }
 
-  //   setMarkedDates(updatedMarkedDates);
-  // }
-
-  //   <Calendar
-  //   onDayPress={(dia) => onDaySelect(dia)}
-  //   markedDates={markedDates}
-  // ></Calendar>
+    setMarkedDates(updatedMarkedDates);
+  }
 
   return (
     <View style={styles.container}>
-
+      <Calendar
+        onDayPress={(dia) => onDaySelect(dia)}
+        markedDates={markedDates}
+      ></Calendar>
     </View>
   );
 }
